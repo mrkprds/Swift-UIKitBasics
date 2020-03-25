cf6bab31ec1d60bed8372ec4ab20ce1722cbb921
Patrick <markpatrickperdon@gmail.com>
2020-03-26 00:55:09 +0800

    created view controller

---
+++ Basics/HardCodeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HardCodeViewController.swift
+//  Basics
+//
+//  Created by Mark Patrick Perdon on 3/25/20.
+//  Copyright © 2020 Mark Patrick Perdon. All rights reserved.
+//
+
+import UIKit
+
+class HardCodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
